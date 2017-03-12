d0b28647e2e2bfa17c61cbcd7196dcfdc765ed4a
= <hannes@publicplan.de>
2017-03-12 12:54:35 +0100

    Manager.js Prototypen

---
+++ Server/manager.js
@@ -0,0 +1,34 @@
+class storageController{
+	constructor(JSONInput){
+		this.JSONObject 	= JSONInput;
+		this.currentLayer	= 0;
+		
+	}
+	
+	//Tut nächstes Spiel laden tun.
+	get nextGame(){
+	
+	}	
+	
+	//Runde beenden
+	gameFinished(){
+	
+	}
+	
+	//Punkt für Team x in Spiel x auf der aktuellen Ebene
+	gamePoint(gameID,teamID){
+	
+	}
+	
+	//Punkt abziehen in Team x in Spiel x auf der aktuellen Ebene
+	gameRemovePoint(gameID,teamID){
+	
+	}
+	
+	//Auf in die nächste Spiel Ebene gehen tun.
+	gameStartNextLayer(){
+		
+	}
+	
+	
+}
